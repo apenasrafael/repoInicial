22cbbca0d445a52575b01c80dba4d5c2e9825dd4
apenasrafael <rafaelzez@gmail.com>
2015-09-09 10:49:01 -0300

    primeiro commit

---
+++ Variavel1.java
@@ -0,0 +1,11 @@
+public class Variavel1{
+	public static void main(String[] args){
+		int quantidadeAlunos=500/3;
+		//quantidadeAlunos=63;
+		int quantidadeAlunosNota10;
+		int numeroDeAlunoAprovados;
+
+		System.out.println(quantidadeAlunos);
+		
+	}
+}
